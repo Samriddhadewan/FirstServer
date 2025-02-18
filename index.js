b23d0025422d2a17621845a7798653dc0224b260
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-19 00:04:20 +0600

    video 2 done

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const phones = require("./phones.json")
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res)=>{
-    res.send("hello world!") 
+    res.send("hello world! soon toon") 
+})
+
+app.get("/phones", (req, res)=>{
+    res.send(phones);
+})
+
+app.get("/phones/:id", (req, res)=> {
+    const id = parseInt(req.params.id);
+    const phone = phones.find(phone => phone.id === id) || {}
+    res.send(phone)
 })
 
 app.get("/data", (req, res)=>{
-    res.send("more data coming soon")
+    res.send("more data coming soon tooon")
 })
-
 app.listen(port, ()=>{
     console.log(`my first server is running on port${port}`);
 })
